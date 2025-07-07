0ff1ea127a7363ee93b4a9e5c98824e20d9393f3
isahilmishra <isahilmishra0175@gmail.com>
2025-07-08 02:10:53 +0530

    Time: 19 ms (5.11%), Space: 13.1 MB (53.51%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+         unordered_map<char, int> roman = {
+        {'I', 1},   {'V', 5},   {'X', 10},
+        {'L', 50},  {'C', 100}, {'D', 500},
+        {'M', 1000}
+    };
+
+    int total = 0;
+    int n = s.size();
+
+    for (int i = 0; i < n; ++i) {
+        int value = roman[s[i]];
+        // If the next numeral is larger, subtract this one
+        if (i + 1 < n && roman[s[i]] < roman[s[i + 1]]) {
+            total -= value;
+        } else {
+            total += value;
+        }
+    }
+
+    return total;
+    }
+};
