a0a488018290b7c465f28dce7141c91193ff87ad
isahilmishra <isahilmishra0175@gmail.com>
2025-07-22 02:11:24 +0530

    Time: 0 ms (100%), Space: 10.6 MB (23.19%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+ int findSubarray(vector<int> &arr, int cap){
+    int cnt=1; int indexUsed=0;
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i]+indexUsed<=cap){
+            indexUsed+=arr[i];            
+        }
+        else{
+            cnt++;
+            indexUsed=arr[i];
+        }
+    }
+    return cnt;
+}
+    int splitArray(vector<int>& a, int k) {
+         int n= a.size();
+    if(k>n) return -1;
+    int low= *max_element(a.begin(),a.end());
+    int high= accumulate(a.begin(),a.end(),0);
+    while(low<=high){
+        int mid=(low+high)/2;
+        int cnt= findSubarray(a,mid);
+        if(cnt>k) low=mid+1;
+        else high=mid-1; 
+    }
+    return low;
+    }
+};
