3488c0035fb34ef9c2d8e3136c8d8fb974d57851
isahilmishra <isahilmishra0175@gmail.com>
2025-07-29 02:58:09 +0530

    Time: 0 ms (100%), Space: 8.6 MB (70.49%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N=n;
+        double ans=1.0;
+       
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        while(N>0){
+            if(N%2==1){
+                ans=ans*x;
+                N=N-1;
+            }
+            else{
+                N=N/2;
+                x=x*x;
+            }
+        }
+       return ans;
+    }
+};
