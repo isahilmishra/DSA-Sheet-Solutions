10dd84d841b8c69908312a8afca8637d7b942297
isahilmishra <isahilmishra0175@gmail.com>
2025-08-05 04:10:45 +0530

    Time: 8 ms (52.36%), Space: 18.2 MB (64.01%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -16,30 +16,22 @@ public:
         return maxArea;
     }
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> left(n), right(n, n);
-        stack<int> st;
-
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
-            left[i] = st.empty() ? -1 : st.top();
-            st.push(i);
+      stack<int> st; int maxArea=0;
+      for(int i=0; i<heights.size(); i++){
+        while(!st.empty()&& heights[st.top()]>heights[i]){
+            int element=st.top();
+            st.pop();
+            int nse=i; int pse= st.empty()? -1 : st.top();
+            maxArea= max( maxArea, (nse-pse-1)*heights[element]);
         }
-
-        st = stack<int>();
-
-        for (int i = n - 1; i >= 0; --i) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
-            right[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-
-        int maxArea = 0;
-        for (int i = 0; i < n; ++i)
-            maxArea = max(maxArea, heights[i] * (right[i] - left[i] - 1));
-
-        return maxArea;
+        st.push(i);
+      }
+      while(!st.empty()){
+       int nse=heights.size();
+       int element=st.top(); st.pop();
+       int pse= st.empty()? -1 : st.top();
+         maxArea= max( maxArea, (nse-pse-1)*heights[element]);
+      }
+      return maxArea;
     }
 };
