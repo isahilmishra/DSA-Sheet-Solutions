baaed2ba12a016abc172a10f49bfae48d71e5da0
isahilmishra <isahilmishra0175@gmail.com>
2025-08-06 22:41:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Length of the longest substring/length-of-the-longest-substring.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int longestUniqueSubstring(string &input) {
+        // code
+        vector<int> hashMap(256,-1);
+         int n= input.size();
+         int l=0, r=0; int maxLen=0;
+         while(r<n){
+            if(hashMap[input[r]]!=-1){
+                if(hashMap[input[r]]>=l){
+                    l=hashMap[input[r]]+1;
+                }
+            }
+            int len=r-l+1;
+            maxLen= max(len, maxLen);
+            hashMap[input[r]]=r;
+            r++;
+         }
+         return maxLen;
+    }
+};
