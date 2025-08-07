36a21ceb05705306f8adf83273424eef0de27af4
isahilmishra <isahilmishra0175@gmail.com>
2025-08-08 03:14:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Character Replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int characterReplacement(string& s, int k) {
+        // code here
+        int l=0,r=0,maxLen=0, maxFreq=0;
+       int hash[26]={0};
+       while(r<s.size()){
+        hash[s[r]-'A']++;
+        maxFreq=max(maxFreq, hash[s[r]-'A']);
+        int size= r-l+1;
+        if(size-maxFreq>k){
+            hash[s[l]-'A']--; 
+            l++;
+        }
+        if(size-maxFreq<=k){
+            maxLen=max(maxLen,size);
+        }
+        r++;
+       }
+       return maxLen;
+    }
+};
