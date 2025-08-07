0c617b9d5f7aa007f7fab2bc1d0ac5274004fafb
isahilmishra <isahilmishra0175@gmail.com>
2025-08-08 02:15:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Substring/count-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int countSubstring(string s) {
+        // Code here
+         int lastSeen[3]={-1,-1,-1};
+        int cnt=0;
+        for(int i=0; i<s.size();i++){
+            lastSeen[s[i]-'a']=i;
+            cnt=(cnt+1)+min({lastSeen[0], lastSeen[1],lastSeen[2]});
+        }
+        return cnt;
+    }
+};
