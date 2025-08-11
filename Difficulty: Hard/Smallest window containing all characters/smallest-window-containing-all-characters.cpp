6a47c0e322c72996a1af937da8f8554d1e4e9407
isahilmishra <isahilmishra0175@gmail.com>
2025-08-12 02:11:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    string smallestWindow(string &s, string &t) {
+        //  code here
+         int minLen= INT_MAX;
+        if(s.empty() || t.empty()) return "";
+        int hash[126]={0};
+        for(int i=0; i<t.size(); i++){
+            hash[t[i]]++;
+        }
+        int l=0,r=0, cnt=0; int sIndex=-1;
+        while(r<s.size()){
+            if(hash[s[r]]>0) cnt++;
+            hash[s[r]]--;
+            while(cnt==t.size()){
+               if(r-l+1<minLen){
+                  minLen=r-l+1;
+                  sIndex=l;
+               }
+               hash[s[l]]++;
+               if(hash[s[l]]>0) cnt--;
+               l++;
+            }
+            r++;
+        }
+        return sIndex==-1? "" : s.substr(sIndex, minLen);
+    }
+};
