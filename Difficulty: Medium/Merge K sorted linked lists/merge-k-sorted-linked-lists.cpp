ceeede6e9f9611c106fdb9b8e3b952400f0e2d5b
isahilmishra <isahilmishra0175@gmail.com>
2025-08-13 03:11:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
@@ -0,0 +1,44 @@
+/*Linked list Node structure
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    Node* mergeKLists(vector<Node*>& listArray) {
+        // Your code here
+         priority_queue<pair<int, Node*>,
+                  vector<pair<int, Node*>>,greater<pair<int,Node*>>> pq;
+    
+    for(int i=0; i<listArray.size(); ++i){
+        if(listArray[i]){
+            pq.push({listArray[i]->data, listArray[i]});
+        }
+    }
+    Node* dNode= new Node(-1);
+    Node* temp= dNode;
+    while(!pq.empty()){
+        auto it = pq.top();
+        pq.pop();
+        
+        Node* node = it.second;
+        temp->next = node;
+        temp = temp->next;
+
+        if (node->next) {
+            pq.push({node->next->data, node->next});
+        }
+    }
+    return dNode->next;
+    }
+};
