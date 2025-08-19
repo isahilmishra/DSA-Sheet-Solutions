9234e4a14f17752715f52ca0136a30d2cf9561f3
isahilmishra <isahilmishra0175@gmail.com>
2025-08-19 07:09:53 +0530

    Time: 0 ms (100%), Space: 9 MB (82.07%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    int helper(string &s, int i, int n, long num, int sign) {
+        // base case: if out of bounds or non-digit
+        if (i >= n || !isdigit(s[i])) return (int)(num * sign);
+
+        num = num * 10 + (s[i] - '0');
+
+        if (num * sign > INT_MAX) return INT_MAX;
+        if (num * sign < INT_MIN) return INT_MIN;
+
+        return helper(s, i + 1, n, num, sign);
+    }
     int myAtoi(string s) {
         int i=0, n=s.size();
         //Skip the leading spaces;
@@ -10,14 +21,6 @@ public:
             sign= (s[i]=='-')? -1 : 1;
             i++;
         }
-        long num=0;
-        while(i<n && isdigit(s[i])){
-            num=num*10 +(s[i]-'0');
-
-            if(num*sign >INT_MAX) return INT_MAX;
-            if(num*sign< INT_MIN) return INT_MIN;
-            i++;
-        }
-        return (num*sign);
+        return helper(s,i,n,0,sign);
     }
 };
