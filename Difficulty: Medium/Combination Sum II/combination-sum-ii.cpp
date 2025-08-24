f75e892ab21bd7a429fbd1aa26a3c451ec7259e8
isahilmishra <isahilmishra0175@gmail.com>
2025-08-24 23:33:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Combination Sum II/combination-sum-ii.cpp
@@ -0,0 +1,29 @@
+// User function Template for C++
+
+class Solution {
+  public:
+     void findCombinations(int ind, vector<int> &candidates, int target,
+                         vector<vector<int>> &ans, vector<int> &ds){
+                            if(target==0){
+                                ans.push_back(ds);
+                                return;
+                            }
+                            for(int i=ind; i<candidates.size(); i++){
+                                if(i>ind && candidates[i]==candidates[i-1]) continue;
+                                if(candidates[i]>target) break;
+                            ds.push_back(candidates[i]);
+                            findCombinations(i+1,candidates, target-candidates[i],ans,ds);
+                            ds.pop_back();
+                            }
+
+                         }
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+        // Write your code here
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>ans;
+        vector<int> ds;
+        findCombinations(0, candidates, target, ans, ds);
+        return ans;
+        
+    }
+};
